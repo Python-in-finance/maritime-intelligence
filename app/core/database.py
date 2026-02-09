71a4731e72d1f799fac06388ca9781c4463bd998
Python-in-finance <121171692+Python-in-finance@users.noreply.github.com>
2026-02-09 23:35:28 +0000

    Create database.py

---
+++ app/core/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+
+engine = create_engine("sqlite:///./maritime.db", connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def check_db_connection():
+    try:
+        with engine.connect() as conn:
+            conn.execute("SELECT 1")
+        return True
+    except:
+        return False
