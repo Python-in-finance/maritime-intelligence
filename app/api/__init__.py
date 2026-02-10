77ca3e0c92e8b7f5a1d3ede24856c28fab82053c
Python-in-finance <121171692+Python-in-finance@users.noreply.github.com>
2026-02-10 14:08:40 +0000

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 from app.api.vessels import router as vessels_router
 
-api_router = APIRouter(prefix="/api/v1")
+api_router = APIRouter()
 api_router.include_router(vessels_router, prefix="/vessels", tags=["vessels"])
